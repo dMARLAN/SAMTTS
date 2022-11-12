8433d4746e8fb4f14da4ec9e058a53e26ba8a551
MARLAN <cpenarsky@shaw.ca>
2022-11-11 22:37:55 -0600

    add warning control

--- SAMTTSInit.lua
+++ SAMTTSInit.lua
@@ -2,4 +2,6 @@ SAMTTS.addSAM("CSGAD1", "KILO")
 SAMTTS.addSAM("CSGAD2", "LIMA")
 SAMTTS.addSAM("PAT1", "PAPA")
 
-SAMTTS.setCoalitionParams("BLUE", "255,262,259,268", "AM,AM,AM,AM", "male", "en-US", "en-US-Standard-J", true)
+SAMTTS.setCoalitionParams("BLUE", "251,255,262,259,268", "AM,AM,AM,AM,AM", "male", "en-US", "en-US-Standard-J", true)
+
+SAMTTS.setWarningControlCallsign("BLUE", "DEEP-SEA")
