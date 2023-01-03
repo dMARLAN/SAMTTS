30917779c44fc7b4cc13f74a2f22a85d8928ff0b
Chad <cpenarsky@shaw.ca>
2023-01-02 18:57:27 -0600

    adjust example

--- SAMTTSInit.lua
+++ SAMTTSInit.lua
@@ -3,12 +3,12 @@ SAMTTS.googleTTS = true
 SAMTTS.debug = true
 
 local freqs = "257,251,255,262,259,268"
-local modulation = "AM, AM, AM, AM, AM, AM"
+local modulation = "AM,AM,AM,AM,AM,AM"
 
-SAMTTS.addSAM("CSGAD NORTH (Kilo)", "KILO", "BLUE", freqs, modulation)
-SAMTTS.addSAM("CSGAD SOUTH (Lima)", "LIMA", "BLUE", freqs, modulation)
-SAMTTS.addSAM("BLUE-SAM Patriot (Papa)", "PAPA", "BLUE", freqs, modulation)
-SAMTTS.addSAM("BLUE-SAM NASAMS Hatay (Hotel)", "HOTEL", "BLUE", freqs, modulation)
-SAMTTS.addSAM("BLUE-SAM Patriot (Charlie)", "CHARLIE", "BLUE", freqs, modulation)
+SAMTTS.addSAM("CSGAD-N", "KILO", "BLUE", freqs, modulation)
+SAMTTS.addSAM("CSGAD-S", "LIMA", "BLUE", freqs, modulation)
+SAMTTS.addSAM("BLUE-SAM Patriot (Papa)-1", "PAPA", "BLUE", freqs, modulation)
+SAMTTS.addSAM("BLUE-SAM NASAMS Hatay (Hotel)-1", "HOTEL", "BLUE", freqs, modulation)
+SAMTTS.addSAM("BLUE-SAM Patriot (Charlie)-1", "CHARLIE", "BLUE", freqs, modulation)
 
 SAMTTS.addWarningController("DSEA1", "DEEP SEA", "BLUE", freqs, modulation)
